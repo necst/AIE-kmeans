bdbcd0e500239a883e392bf01f2a732b3e31ef2c
Eleonora Cabai <eleonora.cabai@mail.polimi.it>
2025-01-27 21:45:32 -0600

    added mac queen implementation

--- MacQueen/floats/common/constants.h
+++ MacQueen/floats/common/constants.h
@@ -1,14 +1,16 @@
 /***************************************************************
-*
-* Configuration header file for Versal 3D Image Registration
-*
-****************************************************************/
+ *
+ * Configuration header file for Versal 3D Image Registration
+ *
+ ****************************************************************/
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
-typedef float data_t;
-#define MAX_CLUSTERS 16
+#define MAX_CLUSTERS 32
 
-#define OFFSET 10000
+// For each point we read 2 coordinates (x, y)
+#define COORDS_IN 8
+#define POINTS 4
+#define POINTS_LOG 2
 
 #endif
